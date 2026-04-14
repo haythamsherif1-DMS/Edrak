71eef71bb00b6a9b1be5aacab167d4ec7664b414
haythamsherif1-DMS <haythamsherif1@gmail.com>
2026-04-14 13:36:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'edrak-cache-v1.29';
 const CACHE_NAME = 'edrak-cache-v1.29'; // ارفع الإصدار هنا أيضاً
 const urlsToCache = [
   './index.html?v=1.29',
-  './manifest.json?v=1.28',
+  './manifest.json?v=1.29',
   './script.js?v=1.29',
   './icon.png?v=1.29', // أضف الأيقونة هنا برقم إصدار جديد
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
